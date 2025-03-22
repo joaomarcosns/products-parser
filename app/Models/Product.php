45023feec19fa65a2ba8b645a11a9e564033780f
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2025-03-22 15:50:51 -0300

    fix: refactor product model

---
+++ app/Models/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ProductStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'status',
+        'url',
+        'creator',
+        'created_t',
+        'last_modified_t',
+        'product_name',
+        'quantity',
+        'brands',
+        'categories',
+        'labels',
+        'cities',
+        'purchase_places',
+        'stores',
+        'ingredients_text',
+        'traces',
+        'serving_size',
+        'serving_quantity',
+        'nutriscore_score',
+        'nutriscore_grade',
+        'main_category',
+        'image_url',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => ProductStatusEnum::class,
+        ];
+    }
+}
