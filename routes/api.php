5f35b0dda36963655a48084d734f8ccd963fe0de
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2025-03-22 14:13:37 -0300

    chore: add api version

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Products API',
+            'status' => 'Connected',
+            'version' => config('app.version')
+        ]);
+    });
+
+
+    Route::apiResource('products', ProductsController::class)->except('store');
+});
