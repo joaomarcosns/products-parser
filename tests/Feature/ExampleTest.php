564bd66d983057a48780063240f12a617c4d28b3
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2025-03-22 13:48:54 -0300

    chore: add pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,8 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+test('sum', function () {
+    $result = 1 + 2;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+    expect($result)->toBe(3);
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,7 @@
 <?php
 
-namespace Tests\Unit;
+test('sum', function () {
+    $result = 1 + 2;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+    expect($result)->toBe(3);
+});
