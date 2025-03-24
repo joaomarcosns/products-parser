0f39d2057a924386105bf3a47971106303245bfc
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2025-03-23 21:54:00 -0300

    fix: change schedule interval

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new ImportFoodDataJob())->everySecond();
+Schedule::job(new ImportFoodDataJob())->daily();
